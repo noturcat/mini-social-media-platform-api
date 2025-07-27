d75e494168df917887dd0ccb93a54390679cb529
noturcat <chez.cus@gmail.com>
2025-07-28 04:16:40 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000','https://mini-social-media-platform.vercel.app'],
     'allowed_headers' => ['*'],
     'supports_credentials' => false, // false for token-based auth
 ];
